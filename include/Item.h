1149f27cb651e9be68404dc48dbbe118742372c1
Vlasov Daniil <356550@edu.itmo.ru>
2025-09-25 19:56:20 +0300

    feat(Item.h): add description field for museum display

--- include/Item.h
+++ include/Item.h
@@ -7,8 +7,10 @@ struct Item {
     std::string name;
     std::string type;
     int value;
+    std::string description;
 
-    Item(const std::string& n, const std::string& t, int v) : name(n), type(t), value(v) {}
+    Item(const std::string& n, const std::string& t, int v, const std::string& d = "")
+        : name(n), type(t), value(v), description(d) {}
 };
 
 #endif // ITEM_H
